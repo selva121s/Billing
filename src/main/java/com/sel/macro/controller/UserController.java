df3fd57d66f42afe7a46c766620d05cbbdfdcefe
selvam selladurai <selva121s@gmail.com>
2023-01-24 21:55:50 +0530

    adding slf4j

--- src/main/java/com/sel/macro/controller/UserController.java
+++ src/main/java/com/sel/macro/controller/UserController.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.sel.macro.controller;
 
 import java.time.Instant;
@@ -27,14 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sel.macro.model.User;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class UserController {
 	
 	@GetMapping(path="/user")
 	public User getUser(@RequestParam(name = "id",required=true) Long id) {
+		log.info("inside getUser...");
 		User user=new User();
 		user.setId(id);
-		user.setName("selvam SELLADURAI"+id);
+		user.setName(id+" - selvam SELLADURAI");
 		user.setEmail("selva121s@gmail.com");
 		user.setAddress("3/29 Middle Street,P K Agaram, Lalgudi Tk, Trichy Dt");
 		user.setLoggedIn(Date.from(Instant.now()));
